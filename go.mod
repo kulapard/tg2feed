a47bd1c59d2d0d8df7351f7c1224c680c2f610f7
Taras Drapalyuk <taras@drapalyuk.com>
2023-12-11 12:25:38 +0400

    add code

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/kulapard/tg2rss
+
+go 1.21
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.1
+	github.com/foolin/pagser v0.1.6
+	github.com/gorilla/feeds v1.1.2
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/rogpeppe/go-internal v1.11.0 // indirect
+	github.com/spf13/cast v1.5.1 // indirect
+	golang.org/x/net v0.19.0 // indirect
+)
---
+++ vendor/github.com/foolin/pagser/config.go
@@ -0,0 +1,29 @@
+package pagser
+
+const ignoreSymbol = "-"
+
+// Config configuration
+type Config struct {
+	TagName    string //struct tag name, default is `pagser`
+	FuncSymbol string //Function symbol, default is `->`
+	CastError  bool   //Returns an error when the type cannot be converted, default is `false`
+	Debug      bool   //Debug mode, debug will print some log, default is `false`
+}
+
+var defaultCfg = Config{
+	TagName:    "pagser",
+	FuncSymbol: "->",
+	CastError:  false,
+	Debug:      false,
+}
+
+// DefaultConfig the default Config
+//	Config{
+//		TagName:    "pagser",
+//		FuncSymbol: "->",
+//		CastError:  false,
+//		Debug:      false,
+//	}
+func DefaultConfig() Config {
+	return defaultCfg
+}

c80c75d1656d3d2bf118175da3774b10eb81a0a9
Taras Drapalyuk <taras@drapalyuk.com>
2023-12-15 19:35:07 +0400

    small refactor (#3)

--- app/main.go
+++ app/main.go
@@ -14,61 +14,59 @@ var revision = "unknown"
 
 const defaultOutputFile = "rss.xml"
 
-// Str2Bool converts string to bool
-func Str2Bool(str string) bool {
-	return str == "true"
+// Config represents application configuration
+type Config struct {
+	OutputFile       string
+	TelegramChannels []string
+}
+
+func (c *Config) String() string {
+	return fmt.Sprintf("OutputFile: %s\nTelegramChannels: %v", c.OutputFile, c.TelegramChannels)
+}
+
+func getConfig() *Config {
+	// Set default output file
+	outfile := os.Getenv("INPUT_OUTPUT-FILE")
+	if outfile == "" {
+		outfile = defaultOutputFile
+	}
+	return &Config{
+		OutputFile:       outfile,
+		TelegramChannels: strings.Split(os.Getenv("INPUT_TELEGRAM-CHANNELS"), ","),
+	}
 }
 
 func main() {
 	fmt.Println("Running tg2rss " + revision)
+	cfg := getConfig()
 
-	// Print all environment variables
-	for _, e := range os.Environ() {
-		pair := strings.Split(e, "=")
-		Info(pair[0] + "=" + pair[1])
-	}
-
-	dryRun := Str2Bool(os.Getenv("INPUT_DRY-RUN"))
-	outputFile := os.Getenv("INPUT_OUTPUT-FILE")
-	tgChannelsStr := os.Getenv("INPUT_TELEGRAM-CHANNELS")
-
-	if dryRun {
-		Info("Dry run")
-	}
-	Info("Output file: " + outputFile)
-	Info("Telegram channel: " + tgChannelsStr)
-
-	// Split channel name by comma
-	tgChannels := strings.Split(tgChannelsStr, ",")
+	// Print config
+	Info(cfg.String())
 
 	var tgFeed *feeds.Feed
-	tgFeeds := make([]*feeds.Feed, len(tgChannels))
+	tgFeeds := make([]*feeds.Feed, len(cfg.TelegramChannels))
 
 	// Build RSS feed for each channel
-	for i, tgChannel := range tgChannels {
-		Info("Telegram channel: " + tgChannel)
+	for i, tgChannel := range cfg.TelegramChannels {
+		Info("Building RSS feed for Telegram channel: " + tgChannel)
 		// Parse the page
 		page := parser.Parse(tgChannel)
 		tgFeeds[i] = feed.GetFeed(&page)
 	}
 
+	// Merge all feeds if there are more than one
 	if len(tgFeeds) > 1 {
 		// Merge all feeds
 		tgFeed = feed.Merge(tgFeeds)
+		Info(fmt.Sprintf("Merged %d RSS feeds", len(tgFeeds)))
 	} else {
 		tgFeed = tgFeeds[0]
 	}
-	// Set default output file
-	if outputFile == "" {
-		Warning("Output file is not set. Using default: " + defaultOutputFile)
-		outputFile = defaultOutputFile
-	}
 
 	// Save RSS feed to file
-	if !dryRun {
-		err := feed.SaveToFile(tgFeed, outputFile)
-		if err != nil {
-			log.Fatal(err)
-		}
+	err := feed.SaveToFile(tgFeed, cfg.OutputFile)
+	if err != nil {
+		log.Fatal(err)
 	}
+	Info("RSS feed saved to file: " + cfg.OutputFile)
 }

c744f4dfe0e4b1ec0fb19750e61b1b2125391253
Taras Drapalyuk <taras@drapalyuk.com>
2023-12-15 19:33:09 +0400

    refactor logs

--- app/main.go
+++ app/main.go
@@ -16,18 +16,12 @@ const defaultOutputFile = "rss.xml"
 
 // Config represents application configuration
 type Config struct {
-	DryRun           bool
 	OutputFile       string
 	TelegramChannels []string
 }
 
 func (c *Config) String() string {
-	return fmt.Sprintf("DryRun: %v\nOutputFile: %s\nTelegramChannels: %v", c.DryRun, c.OutputFile, c.TelegramChannels)
-}
-
-// str2Bool converts string to bool
-func str2Bool(str string) bool {
-	return str == "true"
+	return fmt.Sprintf("OutputFile: %s\nTelegramChannels: %v", c.OutputFile, c.TelegramChannels)
 }
 
 func getConfig() *Config {
@@ -37,7 +31,6 @@ func getConfig() *Config {
 		outfile = defaultOutputFile
 	}
 	return &Config{
-		DryRun:           str2Bool(os.Getenv("INPUT_DRY-RUN")),
 		OutputFile:       outfile,
 		TelegramChannels: strings.Split(os.Getenv("INPUT_TELEGRAM-CHANNELS"), ","),
 	}
@@ -55,24 +48,25 @@ func main() {
 
 	// Build RSS feed for each channel
 	for i, tgChannel := range cfg.TelegramChannels {
-		Info("Telegram channel: " + tgChannel)
+		Info("Building RSS feed for Telegram channel: " + tgChannel)
 		// Parse the page
 		page := parser.Parse(tgChannel)
 		tgFeeds[i] = feed.GetFeed(&page)
 	}
 
+	// Merge all feeds if there are more than one
 	if len(tgFeeds) > 1 {
 		// Merge all feeds
 		tgFeed = feed.Merge(tgFeeds)
+		Info(fmt.Sprintf("Merged %d RSS feeds", len(tgFeeds)))
 	} else {
 		tgFeed = tgFeeds[0]
 	}
 
 	// Save RSS feed to file
-	if !cfg.DryRun {
-		err := feed.SaveToFile(tgFeed, cfg.OutputFile)
-		if err != nil {
-			log.Fatal(err)
-		}
+	err := feed.SaveToFile(tgFeed, cfg.OutputFile)
+	if err != nil {
+		log.Fatal(err)
 	}
+	Info("RSS feed saved to file: " + cfg.OutputFile)
 }
